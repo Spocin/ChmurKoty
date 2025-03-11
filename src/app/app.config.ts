8adfd059ebf31fb368054bbb91350980074fd297
Michał Spociński <michal.spocinski.01@gmail.com>
2025-03-11 20:37:11 +0100

    Configure interopt between Tailwind and PrimeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,6 +18,13 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        options: {
+          darkModeSelector: '.my-app-dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng'
+          }
+        }
       },
     }),
     provideClientHydration(withEventReplay()),
