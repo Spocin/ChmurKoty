d6b4cca9c800bbe9f5dfc933bfd3a448aca1e4d4
Michał Spociński <michal.spocinski.01@gmail.com>
2025-03-12 02:59:17 +0100

    Usage of custom PrimeNG Preset and Sen font

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,15 +4,15 @@ import { appRoutes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 import { DataAccessThemesServiceService } from '@chmur-koty/data-access-themes-service';
+import { ChmurKotyPreset } from '@chmur-koty/ui-chmur-koty-preset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: ChmurKotyPreset,
         options: {
           darkModeSelector: `.${DataAccessThemesServiceService.darkModeClassName}`,
           cssLayer: {
---
+++ src/libs/ui/ChmurKotyPreset/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/ui-chmur-koty-preset/ChmurKotyPreset';
