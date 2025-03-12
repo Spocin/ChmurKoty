45858e93999a4475a17543aca53e8427ef80850a
Michał Spociński <michal.spocinski.01@gmail.com>
2025-03-12 21:27:10 +0100

    Basic template

--- src/libs/ui/login-panel/src/lib/ui-login-panel/ui-login-panel.component.ts
+++ src/libs/ui/login-panel/src/lib/ui-login-panel/ui-login-panel.component.ts
@@ -1,10 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Panel } from 'primeng/panel';
+import { PrimeTemplate } from 'primeng/api';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FloatLabel } from 'primeng/floatlabel';
+import { InputText } from 'primeng/inputtext';
+import { PasswordDirective } from 'primeng/password';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'lib-ui-login-panel',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    Panel,
+    PrimeTemplate,
+    NgOptimizedImage,
+    FloatLabel,
+    InputText,
+    ReactiveFormsModule,
+    PasswordDirective,
+    Button,
+  ],
   templateUrl: './ui-login-panel.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UiLoginPanelComponent {}
+export class UiLoginPanelComponent {
+  private readonly fb = inject(NonNullableFormBuilder);
+
+  protected form = this.fb.group({
+    email: this.fb.control('', [Validators.required, Validators.email]),
+    password: this.fb.control('', [Validators.required]),
+  });
+}
