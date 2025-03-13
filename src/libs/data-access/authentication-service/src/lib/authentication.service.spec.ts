2214c56b271da07eb506366fcc3acb6148483c87
Michał Spociński <michal.spocinski.01@gmail.com>
2025-03-13 14:18:53 +0100

    Fix tests

--- src/libs/data-access/authentication-service/src/lib/authentication.service.spec.ts
+++ src/libs/data-access/authentication-service/src/lib/authentication.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthenticationService } from './authentication.service';
+import { MockProvider } from 'ng-mocks';
+import { MessageService } from 'primeng/api';
+import { APP_CONFIG } from '@chmur-koty/util-environment-config';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [MockProvider(MessageService), MockProvider(APP_CONFIG)],
+    });
     service = TestBed.inject(AuthenticationService);
   });
 
