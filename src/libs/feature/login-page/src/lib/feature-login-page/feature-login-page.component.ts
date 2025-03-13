56a901d682dd75a80a2be275355a9a67e48d89df
Michał Spociński <michal.spocinski.01@gmail.com>
2025-03-13 01:15:12 +0100

    Customize dark mode for login-page

--- src/libs/feature/login-page/src/lib/feature-login-page/feature-login-page.component.ts
+++ src/libs/feature/login-page/src/lib/feature-login-page/feature-login-page.component.ts
@@ -9,14 +9,8 @@ import { FeatureLanguageSwitchComponent } from '@chmur-koty/feature-language-swi
   selector: 'lib-feature-login-page',
   imports: [CommonModule, UiLoginPanelComponent, FeatureThemeSwitchComponent, FeatureLanguageSwitchComponent],
   templateUrl: './feature-login-page.component.html',
+  styleUrl: './feature-login-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styles: [
-    `
-      :host {
-        @apply flex flex-col items-center justify-between w-full h-screen;
-      }
-    `,
-  ],
 })
 export class FeatureLoginPageComponent {
   protected isLogginIn$ = signal(false);
