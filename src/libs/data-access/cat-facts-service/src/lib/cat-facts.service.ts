e253255102c950c2efa09d19debb3783f67edb52
Michał Spociński <michal.spocinski.01@gmail.com>
2025-03-14 02:48:29 +0100

    WIP

--- src/libs/data-access/cat-facts-service/src/lib/cat-facts.service.ts
+++ src/libs/data-access/cat-facts-service/src/lib/cat-facts.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CatFactsService {
-
-  constructor() { }
+  public readonly loadedFacts = new Set<string>(['test1', 'test2', 'test3']);
 }
--- src/libs/feature/cat-facts-scroll-board/src/lib/feature-cat-facts-scroll-board/feature-cat-facts-scroll-board.component.ts
+++ src/libs/feature/cat-facts-scroll-board/src/lib/feature-cat-facts-scroll-board/feature-cat-facts-scroll-board.component.ts
@@ -1,11 +1,51 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, inject, signal, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CatFactsService } from '@chmur-koty/data-access-cat-facts-service';
+import { ScrollPanel } from 'primeng/scrollpanel';
 
 @Component({
   selector: 'lib-feature-cat-facts-scroll-board',
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollPanel],
   templateUrl: './feature-cat-facts-scroll-board.component.html',
   styleUrl: './feature-cat-facts-scroll-board.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FeatureCatFactsScrollBoardComponent {}
+export class FeatureCatFactsScrollBoardComponent implements AfterViewInit {
+  protected readonly catFactsService = inject(CatFactsService);
+
+  protected readonly isLoading = signal(false);
+  protected readonly facts = signal<string[]>([...this.catFactsService.loadedFacts]);
+
+  @ViewChild('scrollPanel')
+  public scrollPanel?: ScrollPanel;
+
+  ngAfterViewInit() {
+    const scrollContent = this.scrollPanel?.contentViewChild?.nativeElement;
+
+    scrollContent.addEventListener('scrollend', this.onScroll);
+  }
+
+  private onScroll = (event: Event) => {
+    const target = event.target as HTMLElement;
+
+    if (target.scrollTop + target.clientHeight >= target.scrollHeight - 40) {
+      this.loadMore();
+    }
+  };
+
+  private loadMore() {
+    if (this.isLoading()) {
+      return;
+    }
+
+    this.isLoading.set(true);
+
+    setTimeout(() => {
+      console.log('ADDED');
+      this.facts.update((prev) => [...prev, `${new Date()}`]);
+      this.isLoading.set(false);
+
+      this.scrollPanel?.refresh();
+    }, 1000);
+  }
+}
